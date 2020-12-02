a69be0e62a7e1055f33fb548b1be0ee10ab1cc87
Thokozile Soko <thokozile.soko@yahoo.com>
2020-12-02 16:36:15 -0500

    I've been working on a for loop for the questions within the object.

--- script.js
+++ script.js
@@ -6,37 +6,81 @@ var secondsDisplay = document.querySelector("#seconds");
 //questions
 
 var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, chD, correct = 0;
+var score = 0;
 
 var questions = [
   {
-      question: "Is JavaScript a case-sensitive language?",
+      question1: "Is JavaScript a case-sensitive language?",
       a: "Yes",
       b: "No",
       c: "Maybe",
       answer: "A"
     },
   {
-      question: "What are the advantages of JavaScript?",
+      question2: "What are the advantages of JavaScript?",
       a: "Less server interaction",
       b: "Immediate feedback to the visitors",
       c: "All of the above",
       answer: "C"
     },
   {
-      question: "In how many ways a JavaScript code can be involved in an HTML file?",
+      question3: "In how many ways a JavaScript code can be involved in an HTML file?",
       a: "1",
       b: "3",
       c: "4",
       answer: "B"
     },
   {
-      question: "The statement (Provide more details on an element like id, type, value etc.) is the definition of what JS term?",
+      question4: "The statement (Provide more details on an element like id, type, value etc.) is the definition of what JS term?",
       a: "Attribute",
       b: "Property",
       c: "Element",
       answer: "A"
     }
   ];
+
+
+
+//for (var i = 0; i < questions.length; i++) {
+ // var result = window.questions.answer;
+  //if (result == questions[i].answer){
+ //   score++;
+ //   alert("Correct");
+ // } else {
+    //alert("Wrong!");
+//  }
+//}
+//alert(" You got " + score + " / " + questions.length);
+
+var fruits = {
+  apple: 28,
+  orange: 17,
+  pear: 54,
+}
+
+
+//const keys = Object.keys(fruits)
+//for (const key of keys) {
+  //console.log(key)
+//}
+
+//const keys = Object.keys(questions)
+//for (const key of keys) {
+  //console.log(key)
+//}
+var entries = Object.entries(questions)
+console.log(entries)
+
+const keys = Object.keys(questions)
+console.log(keys)
+
+const object = { 1: "Is JavaScript a case-sensitive language?" , 2: "What are the advantages of JavaScript?", 3: "In how many ways a JavaScript code can be involved in an HTML file?" };
+
+for (const property in question1) {
+  console.log(`${property}: ${question1[property]}`);
+}
+
+
 
 
 
@@ -53,7 +97,7 @@ var downloadTimer = setInterval(function(){
   timeleft -= 1;
 }, 1000);
 
-start.addEventListener("click",);
+//start.addEventListener("click",);
 
 
 
