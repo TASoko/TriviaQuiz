8551af4525ad8949e5df481b2e653f34fcffab76
Thokozile Soko <thokozile.soko@yahoo.com>
2020-12-02 23:22:05 -0500

    In this draft I've changed the objects and how they are displayed. I have also gotten the start quiz button to start the timer.

--- script.js
+++ script.js
@@ -1,56 +1,58 @@
 //variabls
-var start = document.querySelector("#start");
+var startBtn = document.getElementById("start-button");
 var secondsDisplay = document.querySelector("#seconds");
+var testspace = document.querySelector("#test");
+var timeleft = 120;
+
+console.log("This should be the start button element", startBtn)
 
 
 //questions
 
 var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, chD, correct = 0;
 var score = 0;
+var currentQuestionIndex = 0
 
 var questions = [
   {
-      question1: "Is JavaScript a case-sensitive language?",
-      a: "Yes",
-      b: "No",
-      c: "Maybe",
+      question: "Is JavaScript a case-sensitive language?",
+      choices: ["Yes", "No", "Maybe"],
       answer: "A"
     },
   {
-      question2: "What are the advantages of JavaScript?",
+      question: "What are the advantages of JavaScript?",
       a: "Less server interaction",
       b: "Immediate feedback to the visitors",
       c: "All of the above",
       answer: "C"
     },
   {
-      question3: "In how many ways a JavaScript code can be involved in an HTML file?",
+      question: "In how many ways a JavaScript code can be involved in an HTML file?",
       a: "1",
       b: "3",
       c: "4",
       answer: "B"
     },
   {
-      question4: "The statement (Provide more details on an element like id, type, value etc.) is the definition of what JS term?",
+      question: "The statement (Provide more details on an element like id, type, value etc.) is the definition of what JS term?",
       a: "Attribute",
       b: "Property",
       c: "Element",
       answer: "A"
     }
-  ];
+];
 
 
+var lastQuestionIndex = questions.length - 1;
 
-//for (var i = 0; i < questions.length; i++) {
- // var result = window.questions.answer;
-  //if (result == questions[i].answer){
- //   score++;
- //   alert("Correct");
- // } else {
-    //alert("Wrong!");
-//  }
 //}
 //alert(" You got " + score + " / " + questions.length);
+
+var button = document
+.createElement("button");
+button.setAttribute("class", "answers")
+
+//<button class="answers" data-answer="A">Yes</button>
 
 var fruits = {
   apple: 28,
@@ -76,17 +78,14 @@ console.log(keys)
 
 const object = { 1: "Is JavaScript a case-sensitive language?" , 2: "What are the advantages of JavaScript?", 3: "In how many ways a JavaScript code can be involved in an HTML file?" };
 
-for (const property in question1) {
-  console.log(`${property}: ${question1[property]}`);
-}
 
 
 
 
 
 //Timer 
-
-var timeleft = 120;
+function startQuiz() {
+console.log("Is this working?")
 var downloadTimer = setInterval(function(){
   if(timeleft <= 0){
     clearInterval(downloadTimer);
@@ -97,8 +96,25 @@ var downloadTimer = setInterval(function(){
   timeleft -= 1;
 }, 1000);
 
-//start.addEventListener("click",);
+function checkAnswers() {
+for (var i = 0; i < questions.length; i++) {
+  var result = window.questions.answer;
+if (result == questions[i].answer){
+   score++;
+    alert("Correct");
+  } else {
+    alert("Wrong!");
+  }
+}
+}
+}
 
+var answerButtons = document.querySelector(".answers")
+
+//Andrew Comments
+// Create your answer buttons first
+//It should contain a class called "answers" and an attribute called "data-answer"
+//The value of data-answer should be "A" for the first button, "B" for the second, etc"
 
 
 
@@ -113,3 +129,6 @@ var downloadTimer = setInterval(function(){
 //compare the user choice to the answer
 //counter for the number of correct answers
 //grab the value of their initials and their score and save it to local storage
+
+startBtn.addEventListener("click", startQuiz);
+answerButtons.addEventListener("click", checkAnswer)
