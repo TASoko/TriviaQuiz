dfb6c879313125d4b6a242a437a039f9af3c2b55
Thokozile Soko <thokozile.soko@yahoo.com>
2020-12-03 22:04:42 -0500

    I added an event listener to the generated buttons but only one works for now.

--- script.js
+++ script.js
@@ -2,82 +2,124 @@
 var startBtn = document.getElementById("start-button");
 var secondsDisplay = document.querySelector("#seconds");
 var testspace = document.querySelector("#test");
-var timeleft = 120;
+var timeleft = 60;
+var Btn = document.querySelector(".answers");
+var answerBtn = document.querySelector("data-answer");
 
-console.log("This should be the start button element", startBtn)
 
+console.log("This should be the start button element", startBtn);
 
 //questions
 
-var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, chD, correct = 0;
+var pos = 0,
+  test,
+  test_status,
+  question,
+  choice,
+  choices,
+  chA,
+  chB,
+  chC,
+  chD,
+  correct = 0;
 var score = 0;
-var currentQuestionIndex = 0
+var currentQuestionIndex = 0;
 
 var questions = [
   {
-      question: "Is JavaScript a case-sensitive language?",
-      choices: ["Yes", "No", "Maybe"],
-      answer: "A"
-    },
+    question: "Is JavaScript a case-sensitive language?",
+    choices: ["Yes", "No", "Maybe"],
+    answer: "A",
+  },
   {
-      question: "What are the advantages of JavaScript?",
-      choices: ["Less server interaction",  "Immediate feedback to the visitors", "All of the above"],
-      answer: "C"
-    },
+    question: "What are the advantages of JavaScript?",
+    choices: [
+      "Less server interaction",
+      "Immediate feedback to the visitors",
+      "All of the above",
+    ],
+    answer: "C",
+  },
   {
-      question: "In how many ways a JavaScript code can be involved in an HTML file?",
-      choices: ["1", "3", "4"],
-      answer: "B"
-    },
+    question:
+      "In how many ways a JavaScript code can be involved in an HTML file?",
+    choices: ["1", "3", "4"],
+    answer: "B",
+  },
   {
-      question: "The statement (Provide more details on an element like id, type, value etc.) is the definition of what JS term?",
-      choices: ["Attribute", "Property", "Element"],
-      answer: "A"
-    }
+    question:
+      "The statement (Provide more details on an element like id, type, value etc.) is the definition of what JS term?",
+    choices: ["Attribute", "Property", "Element"],
+    answer: "A",
+  },
 ];
 
 
 var lastQuestionIndex = questions.length - 1;
 
-//}
+
 //alert(" You got " + score + " / " + questions.length);
 
-var button = document.createElement("button");
-button.setAttribute("class", "answers")
+//var button = document.createElement("button");
+//button.setAttribute("class", "answers")
 
 //<button class="answers" data-answer="A">Yes</button>
 
+function buttonClick() {
+  alert ("It works");
+  console.log("It worked");
+};
 
 
 
 
-//Timer 
+//Timer
+
 function startQuiz() {
-console.log("Is this working?")
-var downloadTimer = setInterval(function(){
-  if(timeleft <= 0){
-    clearInterval(downloadTimer);
-    document.getElementById("seconds").innerHTML = "Finished";
-  } else {
-    document.getElementById("seconds").innerHTML = timeleft + " Seconds Remaining";
-  }
-  timeleft -= 1;
-}, 1000);
+  console.log("Is this working?");
+  var downloadTimer = setInterval(function () {
+    if (timeleft <= 0) {
+      clearInterval(downloadTimer);
+      document.getElementById("seconds").innerHTML = "Finished";
+    } else {
+      document.getElementById("seconds").innerHTML =
+        timeleft + " Seconds Remaining";
+    }
+    timeleft -= 1;
+  }, 1000);
+}
+
 
 function checkAnswers() {
-for (var i = 0; i < questions.length; i++) {
-  var result = window.questions.answer;
-if (result == questions[i].answer){
-   score++;
-    alert("Correct");
-  } else {
-    alert("Wrong!");
+  console.log("Is this working too?");
+  choices = document.getElementsByClassName("answers");
+  for (var i = 0; i < choices.length; i++) {
+    if (choices[i].checked) {
+      choice = choices[i].value;
+    }
   }
-}
-}
+
+  //for (var i = 0; i < questions.length; i++) {
+  //var result = window.questions.answer;
+  //if (result == questions[i].answer) {
+  //score++;
+  //alert("Correct");
+  ///} else {
+  //alert("Wrong!");
+  //}
+  // }
 }
 
-var answerButtons = document.querySelector(".answers")
+//document.getElementsByClassName("answers").addEventListener("click", displayDate);
+
+//function display() {
+//document.getElementsByClassName("demo").innerHTML = Date();
+//}
+
+
+
+
+//var answerButtons = document.getElementsByClassName(".answers").getAttribute("data-answers");
 
 //Andrew Comments
 // Create your answer buttons first
@@ -86,17 +128,11 @@ var answerButtons = document.querySelector(".answers")
 
 
 
-//heading and instructions on how to play the game
-//when you hit start quiz the timer should start
-//some type of array that can iterate over for all the questions
-//array of objects
-//object should include 
-//title of question
-//another key for choices
-//answer key
+
 //compare the user choice to the answer
 //counter for the number of correct answers
 //grab the value of their initials and their score and save it to local storage
 
 startBtn.addEventListener("click", startQuiz);
-answerButtons.addEventListener("click", checkAnswer)
+//answerButtons.addEventListener("click", checkAnswer);
+Btn.addEventListener("click", buttonClick);
