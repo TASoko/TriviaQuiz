e26a18fc1924c62250b45b9a1935c41b39cf81cd
Thokozile Soko <thokozile.soko@yahoo.com>
2020-12-04 19:29:57 -0500

    My create answers function loads when the start button is clicked.

--- script.js
+++ script.js
@@ -3,9 +3,12 @@ var startBtn = document.getElementById("start-button");
 var secondsDisplay = document.querySelector("#seconds");
 var testspace = document.querySelector("#test");
 var timeleft = 60;
+var timeDeduct = 10;
 var Btn = document.querySelectorAll(".answers");
 var answerBtn = document.querySelectorAll(".data-answer");
 var submitBtn = document.querySelector(".submit");
+var questionSection = document.getElementById("questionSection");
+var answerSection = document.getElementById("answerSection");
 
 
 console.log("This should be the start button element", startBtn);
@@ -57,58 +60,61 @@ var questions = [
 
 
 
+
 function Results() {
-var lastQuestionIndex = questions.length - 1;
+  var lastQuestionIndex = questions.length - 1;
 
-var selectedanswer;
+  for (var i = 0; i < questions[currentQuestionIndex].answer.length; i++) {
+    //selectedanswer = questions[i].choices[questions[i].selectedIndex].value;
+    questions[currentQuestionIndex].answer[i]
+    //question.textContent = questions[currentQuestionIndex].answers[i]
+    //document.querySelector('#answers').appendChild(question)
+    //if (selectedanswer === Btn) {
+    //score++;
+  }
+}
 
-//for (var i = 0; i < questions.length; i++){
-//selectedanswer = questions[i].choices[questions[i].selectedIndex].value;
-
-  //if (selectedanswer === Btn) {
-  //score++;
- // }
-//}
-
-  alert(" You got " + score + " / " + questions.length);
+  //alert(" You got " + score + " / " + questions.length);
   //var button = document.createElement("button");
   //button.setAttribute("class", "answers")
 
   //<button class="answers" data-answer="A">Yes</button>
-};
+  //};
 
-//Button Clicks [Verified Don't Touch]
-function buttonClick() {
-  alert("It works")
-};
-
-
+  //Button Clicks [Verified Don't Touch]
+  function buttonClick() {
+    alert("It works")
+  };
 
 
-//Timer [Verified Don't Touch]
 
-function startQuiz() {
-  console.log("Is this working?");
-  createAnswers()
-  var downloadTimer = setInterval(function () {
-    if (timeleft <= 0) {
-      clearInterval(downloadTimer);
-      document.getElementById("seconds").innerHTML = "Finished";
-    } else {
-      document.getElementById("seconds").innerHTML =
-        timeleft + " Seconds Remaining";
-    }
-    timeleft -= 1;
-  }, 1000);
 
+  //Timer [Verified Don't Touch]
+
+  function startQuiz() {
+    console.log("Is this working?");
+    createAnswers()
+    var downloadTimer = setInterval(function () {
+      if (timeleft <= 0) {
+        clearInterval(downloadTimer);
+        document.getElementById("seconds").innerHTML = "Finished";
+      } else {
+        document.getElementById("seconds").innerHTML =
+          timeleft + " Seconds Remaining";
+      }
+      timeleft -= 1;
+    }, 1000);
+
+  
+  }
   function createAnswers() {
+    console.log("the create answers works");
     var q = questions[currentQuestionIndex]
     for (var i = 0; i < q.choices.length; i++) {
+      answerBtn,q.choices[i]
       Btn[i].textContent = q.choices[i]
     }
   }
-}
-
 
   //for (var i = 0; i < questions.length; i++) {
   //var result = window.questions.answer;
@@ -120,36 +126,37 @@ function startQuiz() {
   //}
   // }
 
-//document.getElementsByClassName("answers").addEventListener("click", displayDate);
+  //document.getElementsByClassName("answers").addEventListener("click", displayDate);
 
-//function display() {
-//document.getElementsByClassName("demo").innerHTML = Date();
-//}
-
-
-
-
-//var answerButtons = document.getElementsByClassName(".answers").getAttribute("data-answers");
-
-//Andrew Comments
-// Create your answer buttons first
-//It should contain a class called "answers" and an attribute called "data-answer"
-//The value of data-answer should be "A" for the first button, "B" for the second, etc"
+  //function display() {
+  //document.getElementsByClassName("demo").innerHTML = Date();
+  //}
 
 
 
 
-//compare the user choice to the answer
-//counter for the number of correct answers
-//grab the value of their initials and their score and save it to local storage
+  //var answerButtons = document.getElementsByClassName(".answers").getAttribute("data-answers");
 
-startBtn.addEventListener("click", startQuiz);
-//answerButtons.addEventListener("click", checkAnswer);
-//Btn.addEventListener("click", buttonClick);
+  //Andrew Comments
+  // Create your answer buttons first
+  //It should contain a class called "answers" and an attribute called "data-answer"
+  //The value of data-answer should be "A" for the first button, "B" for the second, etc"
 
-for (var i = 0; i < Btn.length; i++) {
-  Btn[i].addEventListener("click", buttonClick)
-  var q = questions[currentQuestionIndex]
-  Btn[i].textContent = q.choices[i]
-}
-submitBtn.addEventListener("click", Results);
+
+
+
+  //compare the user choice to the answer
+  //counter for the number of correct answers
+  //grab the value of their initials and their score and save it to local storage
+
+  startBtn.addEventListener("click", startQuiz);
+  startBtn.addEventListener("click", createAnswers);
+  //answerButtons.addEventListener("click", checkAnswer);
+  //Btn.addEventListener("click", buttonClick);
+
+  for (var i = 0; i < Btn.length; i++) {
+    Btn[i].addEventListener("click", buttonClick)
+    var q = questions[currentQuestionIndex]
+    Btn[i].textContent = q.choices[i]
+  }
+  submitBtn.addEventListener("click", Results);
